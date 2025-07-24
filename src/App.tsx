715723b8f3ca63fdf5f07788513cc65d0c9b8e19
sodiqscript111 <simidusodiqt@gmail.com>
2025-07-24 01:36:56 +0100

    Updating theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
-// src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import SetupPage from '../src/pages/setupPage';
 import ProfilePage from '../src/pages/profilePage';
 import LandingPage from '../src/pages/landingpage.tsx';
 import HomePage from '../src/pages/homepage';
-import AllServices from "../src/pages/AllServices";
-import PrintingHero from "./pages/printingPage.tsx";
-import SocialMediaHero from "./pages/socialpage.tsx";
-import GraphicDesignHero from "./pages/graphics.tsx";
-import BrandingHero from "./pages/branding.tsx";
-import WebsiteHero from "./pages/website.tsx";
-import VideoEditingHero from "./pages/videography.tsx";
-import ScrollToTop from "./ScrollToTop";
-import TagRedirectPage from "./pages/tagRedirectPage.tsx";
+import AllServices from '../src/pages/AllServices';
+import PrintingHero from './pages/printingPage.tsx';
+import SocialMediaHero from './pages/socialpage.tsx';
+import GraphicDesignHero from './pages/graphics.tsx';
+import BrandingHero from './pages/branding.tsx';
+import WebsiteHero from './pages/website.tsx';
+import VideoEditingHero from './pages/videography.tsx';
+import ScrollToTop from './ScrollToTop';
+import TagRedirectPage from './pages/tagRedirectPage.tsx';
 import Navbar from './component/Navbar';
-import OurWorkPage from "./pages/works.tsx";
-import AboutUs  from "./pages/aboutus.tsx";
-import RegisterForm from "./pages/register.tsx";
-import LoginPage from "./pages/login.tsx";
-import Dashboard from "./pages/dashboard.tsx";
-import ForgotPassword from "./pages/forgotPassword.tsx";
-import ResetPassword from "./pages/resetPassword.tsx";
+import OurWorkPage from './pages/works.tsx';
+import AboutUs from './pages/aboutus.tsx';
+import RegisterForm from './pages/register.tsx';
+import LoginPage from './pages/login.tsx';
+import Dashboard from './pages/dashboard.tsx';
+import ForgotPassword from './pages/forgotPassword.tsx';
+import ResetPassword from './pages/resetPassword.tsx';
+
 function App() {
     return (
         <Router>
+            <AppContent />
+        </Router>
+    );
+}
+
+function AppContent() {
+    const location = useLocation();
+    const [showNavbar, setShowNavbar] = useState(true);
+
+    useEffect(() => {
+        // Hide navbar on /u/:slug route
+        setShowNavbar(!location.pathname.startsWith('/u/'));
+    }, [location.pathname]);
+
+    return (
+        <>
+            {showNavbar && <Navbar />}
             <ScrollToTop />
-            <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/nfc-card" element={<LandingPage />} />
@@ -39,15 +56,15 @@ function App() {
                 <Route path="/website-design" element={<WebsiteHero />} />
                 <Route path="/video-editing" element={<VideoEditingHero />} />
                 <Route path="/tag/:uid" element={<TagRedirectPage />} />
-                <Route path="/ourwork" element={<OurWorkPage/>}/>
-                <Route path="about" element={<AboutUs/>}/>
-                <Route path="/register" element={<RegisterForm/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/ourwork" element={<OurWorkPage />} />
+                <Route path="about" element={<AboutUs />} />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/login" element={<LoginPage />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
             </Routes>
-        </Router>
+        </>
     );
 }
 
